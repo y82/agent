9e6de3c7c0b8cc9e901aa110550be4f8e74faa64
Dean Coakley <d.coakley@f5.com>
2024-05-09 13:23:39 +0100

    Add integration test

--- internal/config/config.go
+++ internal/config/config.go
@@ -385,7 +385,7 @@ func getCommand() *Command {
 
 func getCommon() *CommonSettings {
 	return &CommonSettings{
-		InitialInterval:     DefBackoffInitalInterval,
+		InitialInterval:     DefBackoffInitialInterval,
 		MaxInterval:         DefBackoffMaxInterval,
 		MaxElapsedTime:      DefBackoffMaxElapsedTime,
 		RandomizationFactor: DefBackoffRandomizationFactor,
